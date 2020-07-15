e8bcfa65a51a3c4973ca485f39d3662a0d632d0a
chibuzor christian <gigzblog@gmail.com>
2020-07-15 15:32:18 +0100

    version 1

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   await say({
@@ -31,13 +30,11 @@ app.message('hello', async ({ message, say }) => {
 });
 
 app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
   await ack();
   await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
